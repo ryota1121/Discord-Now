2d529420ef1aa8cec9e8477f2d52301016ce75f6
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2024-02-25 16:04:52 +0000

    2024/2/26 1:04 commit

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  def find_or_create_from_auth_hash(auth_hash)
+    user_params = user_params_from_auth_hash(auth_hash)
+    find_or_create_by(uid: user_params[:uid]) do |user|
+      user.update(user_params)
+    end
+  end
+
+private
+
+  def user_params_from_auth_hash(auth_hash)
+    {
+      name: auth_hash.info.name,
+      email: auth_hash.info.email,
+      image: auth_hash.info.image,
+      uid: auth_hash.uid,
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'homes#index'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+
+  resources :sessions, only: %i[create destroy]
+  
+  get 'homes/top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
